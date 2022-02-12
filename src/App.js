d69a3aa6d8c406261df431d533fab9b54c244661
serenity <43002637+Serenity1337@users.noreply.github.com>
2022-02-12 18:09:22 +0200

    finished task

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
+import SecondBg from './components/SecondBg'
+
+function App() {
+  return (
+    <>
+      <SecondBg />
+      <CharacterEditor />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SecondBg/SecondBg.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './SecondBg.module.css'
+
+const SecondBg = () => {
+  return <div className={styles.secondBg}></div>
+}
+
+export default SecondBg
